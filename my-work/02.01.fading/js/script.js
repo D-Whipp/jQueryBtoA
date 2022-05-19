bbcb24c0bb2fd5d518d2b084d9222741d489a13e
David Whipple <dwhipp88@gmail.com>
2022-05-19 01:41:39 -0600

    16 - lightbox

--- my-work/02.01.fading/js/script.js
+++ my-work/02.01.fading/js/script.js
@@ -1,5 +1,3 @@
 $(function () {
-  $('.red-box').fadeTo(1000, 0.2);
-  $('.blue-box').delay(1000).fadeTo(1000, 0.6);
-  $('.green-box').delay(2000).fadeTo(1000, 0.7);
+  $('.lightbox').delay(500).fadeIn(1000);
 });
