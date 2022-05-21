df194479541452afce4159a90ca2a1da6c88000a
David Whipple <dwhipp88@gmail.com>
2022-05-20 20:36:28 -0600

    completed: 26

---
+++ my-work/section-05/js/script.js
@@ -0,0 +1,16 @@
+$(function () {
+  $('.gallery').css('display', 'none');
+
+  let redBox = $('.red-box');
+  //   console.log(redBox.width());
+  redBox.css('background-color', '#AA7700');
+  $('p').css('font-size', '18px');
+  redBox.css('width', '+=20px');
+  let properties = $('p').css(['font-size', 'line-height', 'color']);
+  console.log(properties);
+  console.log(properties['font-size']);
+  //   redBox.css("user-select", "none")
+  redBox.css('user-select', function () {
+    return "none";
+  });
+});
