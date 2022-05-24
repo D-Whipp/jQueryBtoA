235ec4d0bb1844189713268aa50601d96e8d565c
David Whipple <dwhipp88@gmail.com>
2022-05-23 21:16:22 -0600

    completed 29

--- my-work/section-05/js/script.js
+++ my-work/section-05/js/script.js
@@ -1,19 +1,15 @@
 $(function () {
-  // $('a').addClass('fancy-link');
-  // $('p:first').addClass('large emphasize');
+  // text(), html()
+  let firstPara = $('p:first');
+  console.log(firstPara.text());
+  console.log(firstPara.html());
+  console.log($('p').html());
 
-  // $('li li').addClass(function (index) {
-  //   $(this).addClass('item-' + index);
-  // });
-
-  // $('div').addClass(function (index, currentClass) {
-  //   if (currentClass === 'dummy') {
-  //     return 'red-box';
-  //   }
-  // });
-
-  // $('.red-box').removeClass('red-box').addClass('blue-box');
-
-  // mini challenge: select dummy remove dummy from it and add green-box class
-  $('.dummy').removeClass('dummy').addClass('green-box');
+  // firstPara.text("<strong>Hello World!</strong>")
+  // firstPara.html("<strong>Hello World!</strong>")
+  // mini challenge: append some text to first paragraph
+  // my way
+  // firstPara.append('<p>this was just appended.</p>');
+  // his way
+  firstPara.html(firstPara.html() + ' This was just appended.');
 });
