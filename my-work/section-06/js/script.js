296e559d23ab0e098d094977a95115c994c25995
David Whipple <dwhipp88@gmail.com>
2022-05-24 19:01:16 -0600

    completed 32

--- my-work/section-06/js/script.js
+++ my-work/section-06/js/script.js
@@ -1,1 +1,33 @@
-$(function () {});
+$(function () {
+  // 31
+  //   $('#btn-click').click(function (event) {
+  //     console.log(event);
+  //     alert('Button was clicked!');
+  //   });
+  //   mini-challenge: add click event to redbox so it fades to 50% opacity
+  //   $('.red-box').click(function () {
+  //     $('.red-box').css('opacity', '0.5');
+  //   });
+  //   SUCCESS!!
+  //   his way
+  //   $('.red-box').click(function () {
+  //     $('this').fadeTo(500, 0.5);
+  //   });
+  //   if you ever need to trigger your event use
+  //   $('.red-box').click();
+  // 32
+  // :hover
+  //   $('#btn-hover').hover(function () {
+  //     alert("You're hovering over the hover button!");
+  //   });
+  //   mini-challenge: add hover event to greenbox and change it's text to "I was hovered over"
+  //   $('.green-box').hover(function () {
+  //     $(this).html('<em>Hover-mode activated!</em>');
+  //   });
+  //   SUCCESS!!
+  //   his way
+  //   $('.green-box').hover(function () {
+  //     $(this).text('I was hovered');
+  //   });
+  
+});
