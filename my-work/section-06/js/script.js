7d80399856235e4defeb1ce500be03b13a9f179f
David Whipple <dwhipp88@gmail.com>
2022-05-25 14:48:12 -0600

    completed 35

--- my-work/section-06/js/script.js
+++ my-work/section-06/js/script.js
@@ -1,5 +1,5 @@
 $(function () {
-  // 31
+  // *** 31 ***
   //   $('#btn-click').click(function (event) {
   //     console.log(event);
   //     alert('Button was clicked!');
@@ -15,7 +15,7 @@ $(function () {
   //   });
   //   if you ever need to trigger your event use
   //   $('.red-box').click();
-  // 32
+  // *** 32 ***
   // :hover
   //   $('#btn-hover').hover(function () {
   //     alert("You're hovering over the hover button!");
@@ -29,7 +29,7 @@ $(function () {
   //   $('.green-box').hover(function () {
   //     $(this).text('I was hovered');
   //   });
-  //   33
+  //   *** 33 ***
   //   $('#btn-hover').hover(function () {
   //     alert('Button was hovered.');
   //   });
@@ -87,4 +87,50 @@ $(function () {
   //         $(this).attr('src', images[i]);
   //       });
   //     });
+  //   *** 35 ***
+  // .on("click", function(){...})
+  function logEvent() {
+    console.log('Mouse was clicked or a key was pressed.');
+  }
+
+  $('html').on('click keydown', logEvent);
+
+  // mini-challenge: modularize this code
+  // $('.gallery')
+  //   .find('img')
+  //   .on('click', function () {
+  //     i = (i + 1) % images.length;
+  //     $(this).fadeOut(function () {
+  //       $(this).attr('src', images[i]).fadeIn();
+  //     });
+  //   });
+  // let image = $('.gallery').find('img');
+  // let imageGallery = ['path', 'path', 'path'];
+  // let i = 0;
+
+  // function switchImg(insertImageHere) {
+  //   i = (i + 1) % image.length;
+  //   $(this).fadeOut(function () {
+  //     $(this).attr('src', imageGallery[i].fadeIn());
+  //   });
+  // }
+
+  // imageGallery.on('click', switchImg);
+
+  // what he did
+  var images = [
+    'images/laptop-mobile_small.jpg',
+    'images/laptop-on-table_small.jpg',
+    'images/people-office-group-team_small.jpg',
+  ];
+  var i = 0;
+  var galleryImage = $('.gallery').find('img');
+  galleryImage.on('click', switchToNextImage);
+
+  function switchToNextImage() {
+    i = (i + 1) % images.length;
+    galleryImage.fadeOut(function () {
+      galleryImage.attr('src', images[i]).fadeIn();
+    });
+  }
 });
