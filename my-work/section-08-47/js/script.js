c9b66f8eca27c89bc2e52187c1aab59908029178
David Whipple <dwhipp88@gmail.com>
2022-05-28 14:46:30 -0600

    completed 47

--- my-work/section-08-47/js/script.js
+++ my-work/section-08-47/js/script.js
@@ -1,1 +1,28 @@
-$(function () {});
+$(function () {
+  //JSON, $.getJSON()
+  var flickrApiUrl =
+    'https://www.flickr.com/services/feeds/photos_public.gne?jsoncallback=?';
+
+  $.getJSON(flickrApiUrl, {
+    // options...
+    tags: 'sun, beach',
+    tagmode: 'any',
+    format: 'json',
+  })
+    .done(function (response) {
+      // success
+      console.log(response);
+      $.each(response.items, function (index, value) {
+        console.log(value);
+        $('<img>').attr('src', value.media.m).appendTo('#flickr');
+
+        if (index == 5) {
+          return false;
+        }
+      });
+    })
+    .fail(function (response) {
+      // error
+      alert('Ajax call failed.');
+    });
+});
